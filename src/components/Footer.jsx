c5a8bb8e32f96e7825d20f4a7da25612b7170499
sholikul <sholikulardian@gmail.com>
2024-10-25 18:37:49 +0700

    add footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
-// TODO: answer here
+import { Box, Text, Center } from '@chakra-ui/react';
 
 const Footer = () => {
-    return (
-        // TODO: answer here
-    );
+  return (
+    <Box className='footer' background={'blue.600'} color={'white'} py={2}>
+      <Text className='studentName'>
+        <Center>Sholikul Ardian</Center>
+      </Text>
+      <Text className='studentId'>
+        <Center>FS11861819</Center>
+      </Text>
+    </Box>
+  );
 };
 
 export default Footer;
