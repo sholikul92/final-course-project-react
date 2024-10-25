dcc8ae6a7b8d1cfc5bac35908fb07dbff84d9287
sholikul <sholikulardian@gmail.com>
2024-10-25 18:37:23 +0700

    add navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-// TODO: answer here
+import { Link } from 'react-router-dom';
+import { Box, Flex, Text, Button } from '@chakra-ui/react';
 
 const NavBar = () => {
-    return (
-        // TODO: answer here
-    );
+  return (
+    <Box py={2} px={4} bg={'blue.600'} position={'fixed'} top={0} left={0} right={0} zIndex={1}>
+      <Flex justify={'space-between'}>
+        <Text textColor='white' fontSize={'2xl'} fontWeight={'bold'}>
+          Student Portal
+        </Text>
+        <Flex gap={8}>
+          <Button as={Link} to='/student' data-testid='student-page'>
+            All Student
+          </Button>
+          <Button as={Link} to='/add' data-testid='add-page'>
+            Add Student
+          </Button>
+        </Flex>
+      </Flex>
+    </Box>
+  );
 };
 
 export default NavBar;
