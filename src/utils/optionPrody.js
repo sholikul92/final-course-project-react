08c117d788384ab26d9964d53ce656a907920120
sholikul <sholikulardian@gmail.com>
2024-10-26 18:19:22 +0700

    create data optionOrody

---
+++ src/utils/optionPrody.js
@@ -0,0 +1,46 @@
+export const optionProgramStudy = [
+  {
+    name: 'Ekonomi',
+    value: 'Ekonomi',
+  },
+  {
+    name: 'Manajemen',
+    value: 'Manajemen',
+  },
+  {
+    name: 'Akuntansi',
+    value: 'Akuntansi',
+  },
+  {
+    name: 'Administrasi Publik',
+    value: 'Administrasi Publik',
+  },
+  {
+    name: 'Administrasi Bisnis',
+    value: 'Administrasi Bisnis',
+  },
+  {
+    name: 'Hubungan Internasional',
+    value: 'Hubungan Internasional',
+  },
+  {
+    name: 'Teknik Sipil',
+    value: 'Teknik Sipil',
+  },
+  {
+    name: 'Arsitektur',
+    value: 'Arsitektur',
+  },
+  {
+    name: 'Matematika',
+    value: 'Matematika',
+  },
+  {
+    name: 'Fisika',
+    value: 'Fisika',
+  },
+  {
+    name: 'Informatika',
+    value: 'Informatika',
+  },
+];
