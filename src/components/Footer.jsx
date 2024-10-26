8c464af7479a4693022fcf0186f1f838eca1cdea
sholikul <sholikulardian@gmail.com>
2024-10-26 11:47:05 +0700

    add style to tag Box

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box, Text, Center } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
-    <Box className='footer' background={'blue.600'} color={'white'} py={2}>
+    <Box className='footer' background={'blue.600'} color={'white'} py={2} position={'absolute'} bottom={-153} left={0} right={0}>
       <Text className='studentName'>
         <Center>Sholikul Ardian</Center>
       </Text>
