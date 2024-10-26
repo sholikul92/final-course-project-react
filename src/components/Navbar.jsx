9656b64a9646110672343bbd5eacdbffef635612
sholikul <sholikulardian@gmail.com>
2024-10-26 11:46:21 +0700

    change the button tag to a link tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
-import { Link } from 'react-router-dom';
-import { Box, Flex, Text, Button } from '@chakra-ui/react';
+import { Box, Flex, Text, Link } from '@chakra-ui/react';
+import { Link as LinkTo } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <Box py={2} px={4} bg={'blue.600'} position={'fixed'} top={0} left={0} right={0} zIndex={1}>
       <Flex justify={'space-between'}>
-        <Text textColor='white' fontSize={'2xl'} fontWeight={'bold'}>
+        <Text textColor='white' fontSize={'2xl'} fontWeight={'bold'} data-testid='home-page' as={LinkTo} to='/'>
           Student Portal
         </Text>
-        <Flex gap={8}>
-          <Button as={Link} to='/student' data-testid='student-page'>
+        <Flex gap={8} textColor={'white'} alignItems={'center'}>
+          <Link as={LinkTo} to='/student' data-testid='student-page'>
             All Student
-          </Button>
-          <Button as={Link} to='/add' data-testid='add-page'>
+          </Link>
+          <Link as={LinkTo} to='/add' data-testid='add-page'>
             Add Student
-          </Button>
+          </Link>
         </Flex>
       </Flex>
     </Box>
