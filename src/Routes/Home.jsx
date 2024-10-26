f125eeb41acced9d83f30650d17e429001b8012a
sholikul <sholikulardian@gmail.com>
2024-10-26 18:23:47 +0700

    add flex-box and component footer

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,11 +1,17 @@
-import { Button } from '@chakra-ui/react';
+import { Button, Flex, Box } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
-    <Button as={Link} to='student' data-testid='student-btn' variant='outline' colorScheme='blue'>
-      All Student
-    </Button>
+    <Flex direction={'column'} h={'100vh'}>
+      <Flex flex={1} justify={'center'} align={'center'}>
+        <Button as={Link} to='student' data-testid='student-btn' variant='outline' colorScheme='blue'>
+          All Student
+        </Button>
+      </Flex>
+      <Footer />
+    </Flex>
   );
 };
 
