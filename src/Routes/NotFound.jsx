0747d1d54d48c7271d26a54c04ea7559d6f2bd5b
sholikul <sholikulardian@gmail.com>
2024-10-26 18:20:17 +0700

    modified display pages not found

--- src/Routes/NotFound.jsx
+++ src/Routes/NotFound.jsx
@@ -1,10 +1,20 @@
-// TODO: answer here
 import { useNavigate } from 'react-router-dom';
+import { Text, Flex, Button } from '@chakra-ui/react';
 
 const NotFound = () => {
   const navigate = useNavigate();
+  const back = () => {
+    navigate(-1);
+  };
 
-  return <>{/* TODO: answer here */}</>;
+  return (
+    <Flex justify={'center'} alignItems={'center'} h={'100vh'} direction={'column'}>
+      <Text>404 | Not Found</Text>
+      <Button data-testid='back' onClick={back} bg={'gray.200'}>
+        Back
+      </Button>
+    </Flex>
+  );
 };
 
 export default NotFound;
