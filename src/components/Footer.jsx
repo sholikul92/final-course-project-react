70773ac5e3959cc501ebb7368a518425cdc9b9fe
sholikul <sholikulardian@gmail.com>
2024-10-26 15:56:41 +0700

    updating styling on footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box, Text, Center } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
-    <Box className='footer' background={'blue.600'} color={'white'} py={2} position={'absolute'} bottom={-153} left={0} right={0}>
+    <Box className='footer' as='footer' background={'blue.600'} color={'white'} py={2}>
       <Text className='studentName'>
         <Center>Sholikul Ardian</Center>
       </Text>
