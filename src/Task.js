12efad7ae1076a3bd644a32b1c4ed798b136d1f7
sholikul <sholikulardian@gmail.com>
2024-10-28 11:23:14 +0700

    add url project

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = 'YOURDEPLOYURL'; // TODO: replace this
+const url = 'https://final-course-project-react.web.app/'; // TODO: replace this
 const studentName = 'Sholikul Ardian'; // TODO: replace this
 const studentId = 'FS11861819'; // TODO: replace this
 
